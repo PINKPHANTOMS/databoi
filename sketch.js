f6c018b0d051ec9cd8730cfa9faba0c1958cc3c8
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2020-02-25 10:57:31 -0800

    lj

--- sketch.js
+++ sketch.js
@@ -1,268 +1,86 @@
-let tracker;
-let strange;
-let vid;
-let capture;
-let stock;
-let img;
-let black;
-let distance;
-let vid1
-let vid2
-let aud
-var mic
-let fft
-let sight = true
-
-let play = false
-
-function preload() {
-  stock = loadImage('star.jpg');
-  vid1 = createVideo('gif2.mp4')
-  vid2 = createVideo('gif4.mp4')
-  vid = createVideo('gif3.mp4')
-  black = loadImage('black.jpg')
-}
+const AIO_USERNAME = "pinkphantom"
+const AIO_KEY = "aio_YGGw33eM2p1iMwkFsmpIomzj7mBo"
 
 function setup() {
-  
-
-
-  mic = new p5.AudioIn();
-  mic.start();
-  fft = new p5.FFT();
-
-  fft.setInput(mic)
-
-
-    vid.hide()
-    vid1.hide()
-    vid2.hide()
-
-    vid1.loop()
-    vid2.loop()
-    vid.loop()
-
-    createCanvas(1600, 1000, WEBGL)
-
-    // aud = createCapture(AUDIO)
-    // aud.hide()
-
-    // graphics = createGraphics(100, 100)
-
-    // start capturing video
-
-    capture = createCapture(VIDEO);
-    capture.size(1600, 1000);
-    // capture.hide();
-
-    // capture1 = createCapture(VIDEO);
-    // capture1.size(1600, 1000);
-    // capture1.hide();
-
-    frameRate(10)
-
-    // create the tracker
-    tracker = new clm.tracker()
-    tracker.init()
-    tracker.start(capture.elt)
+    let canvas = createCanvas(640, 480)
+    noLoop()
 }
 
+async function draw() { // note "async" keyword
 
-function draw() {   
+    // fetch our data
+    let data = await fetchData("sensor-test")      // note the "await" keyword
+    print(data)
 
-  var vol = mic.getLevel();
+    // re-sort the array by time
+    data.sort((a, b) => (a.created_at > b.created_at) ? 1 : -1)
 
+    // make a new array with just the sensor values
+    // divide by the max value to "normalize" them to the range 0-1
+    let values = []
+    for (let datum of data) {
+        values.push(datum.value / 4095)
+    }
 
+    // make a new array with just the timestamp
+    // this one is trickier to normalize so we'll do it separately
+    let times = []
+    for (let datum of data) {
+        // convert the string into a numerical timestamp
+        let time = Date.parse(datum.created_at) / 1000
+        times.push(time)
+    }
 
+    // normalize the times to between 0 and 1
+    let start_time = min(times)
+    let stop_time = max(times)
+    for (let i=0; i<times.length; i++) {
+        let time = times[i]
+        times[i] = (time - start_time) / (stop_time - start_time)
+    }
 
+    // now we can draw
 
- let positions = tracker.getCurrentPosition()
+    background(255)
 
-    if (positions.length > 0) {
+    // make colors
+    // in this case, we want a line every pixel, and to interpolate between values
+    // colorMode(HSB) // https://p5js.org/reference/#/p5/colorMode
+    for (let i=1; i<values.length; i++) {
+        let x1 = int(times[i-1] * width)
+        let x2 = int(times[i] * width)
+        print(x1, x2)
+        let c1 = color(55, values[i-1] * 255, 255)
+        let c2 = color(55, values[i] * 255, 255)
+        for (let x=x1; x<=x2; x++) {
+            let interpolation = (x-x1) / (x2-x1)
+            print(x, interpolation)
+            let c = lerpColor(c1, c2, interpolation)
+            stroke(c)
+            line(x, 0, x, height)
+        }
+    }
 
-
-        stroke(255)
-        fill(255)
-
-        // draw the data
-        let i = 0;
-        while (i < positions.length - 1) {
-            // ellipse(positions[i][0], positions[i][1], 4, 4)
-            // text(i, positions[i][0], positions[i][1])
-            // line(positions[i][0], positions[i][1], positions[i+1][0], positions[i+1][1])
-            // image(vid, positions[i][0]-strange.width/2, positions[i][1]-strange.height/2, 50, 50)
-        
-            i++
-          }
-
-
-        // overlay eyes
-        let leftEyeX = positions[32][0]
-        let leftEyeY = positions[32][1]
-
-        let rightEyeX = positions[27][0]
-        let rightEyeY = positions[27][1]
-        
-
-
-        // push()
-        // fill(255, 0, 0)
-        // ellipse(leftEyeX, leftEyeY, 20, 20)
-        // ellipse(rightEyeX, rightEyeY, 20, 20)
-        // pop() 
-
-
-
-        // measure distances between features
-        let noseX = positions[62][0]
-        let noseY = positions[62][1]
-
-        let leftNostrilX = positions[43][0]
-        let leftNostrilY = positions[43][1]
-
-        let rightNostrilX = positions[42][0]
-        let rightNostrilY = positions[42][1]
-
-        
-        let distance = dist(leftEyeX, leftEyeY, rightEyeX, rightEyeY) 
-        // let distanceRight = dist(noseX, noseY, rightNostrilX, rightNostrilY)
-
-
-
-      // print(stretch)
-   // distance = distboi;
-    let stretch = (2*distance)+800-118
-
-
-    if(stretch<960){
-
-    let rsphere = pow(1.048,distance)
-
-    texture(black)
-    sphere(rsphere,32,24)
-
-    translate(0,0,-distance)
-
-    strokeWeight(0)
- 
-    translate(0,0,distance-100)
-
-    let photonsphere = rsphere*3/2
-
-
-    texture(stock)
-    // shearX(millis()/10000)
-    // shearY(millis()/1000)
-    // rotateZ(millis()/(vol*1000))
-
-    torus(photonsphere, distance*3,24,16)
-
-
-      translate(0, 0,450)
-      translate(0,0,-350)
-
-      noStroke()
-      texture(stock)
-
-      ellipsoid(800, 800, stretch, 24, 24)
-
-
-
-    }else{
-
-      print(stretch)
-}}
+    // this one is just a breakpoint line, similar to the adafruit feed page
+    // note that to get the y axis right, we have to flip it by subtracting from 1
+    stroke(0)
+    strokeWeight(2)
+    for (let i=1; i<values.length; i++) {   // starting at 1, not 0
+        let x1 = times[i-1] * width
+        let y1 = (1 - values[i-1]) * height
+        let x2 = times[i] * width
+        let y2 = (1 - values[i]) * height
+        line(x1, y1, x2, y2)
+    }
 
 }
 
-
-
-
-
-function mouseClicked() {
-  if(play == false){
-  vid1.play();
-  vid2.play();
-              vid.play();
-play = true;
-
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function notused(){
-
-
-      // get data from tracker
-    let positions = tracker.getCurrentPosition()
-    if (positions.length > 0) {
-
-        stroke(255)
-        fill(255)
-
-        // draw the data
-        let i = 0;
-        while (i < positions.length - 1) {
-            // ellipse(positions[i][0], positions[i][1], 4, 4)
-            // text(i, positions[i][0], positions[i][1])
-            // line(positions[i][0], positions[i][1], positions[i+1][0], positions[i+1][1])
-            // image(vid, positions[i][0]-strange.width/2, positions[i][1]-strange.height/2, 50, 50)
-        
-            i++
-          }
-
-
-        // overlay eyes
-        let leftEyeX = positions[32][0]
-        let leftEyeY = positions[32][1]
-
-        let rightEyeX = positions[27][0]
-        let rightEyeY = positions[27][1]
-        
-
-
-        // push()
-        // fill(255, 0, 0)
-        // ellipse(leftEyeX, leftEyeY, 20, 20)
-        // ellipse(rightEyeX, rightEyeY, 20, 20)
-        // pop()   
-
-
-
-        // measure distances between features
-        let noseX = positions[62][0]
-        let noseY = positions[62][1]
-
-        let leftNostrilX = positions[43][0]
-        let leftNostrilY = positions[43][1]
-
-        let rightNostrilX = positions[42][0]
-        let rightNostrilY = positions[42][1]
-
-        
-        let distboi = dist(leftEyeX, leftEyeY, rightEyeX, rightEyeY) 
-        // let distanceRight = dist(noseX, noseY, rightNostrilX, rightNostrilY)
-
-}
+// this function fetches our data
+async function fetchData(feed) {
+    return await new Promise((resolve, reject) => {
+        let url = `https://io.adafruit.com/api/v2/${AIO_USERNAME}/feeds/${feed}/data`
+        httpGet(url, 'json', false, function(data) {
+            resolve(data)
+        })
+    })
 }
